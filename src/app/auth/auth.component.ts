475e2d222be17424a829cfcefb2033035f3ee629
sagarmohapatra <sagarkumarmohapatra5@gamail.com>
2024-11-07 11:45:17 +0530

    add sigin

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { FormControl, NgForm, NgModel } from '@angular/forms';
 import { FirestoreService } from './firebase.service';
+import { Observable } from 'rxjs';
+ 
+// import { AuthResponseData } from '../auth/auth-response.model';
 
 @Component({
   selector: 'app-auth',
@@ -10,8 +13,11 @@ import { FirestoreService } from './firebase.service';
 export class AuthComponent {
   isLoginMode: any = true;
   isLoading = false;
-  error:any;
+  error: any;
+   authObs:any= Observable<any>;
+
   constructor(private authService: FirestoreService) {}
+  
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
   }
@@ -20,24 +26,34 @@ export class AuthComponent {
       return;
     }
     this.isLoading = true;
-
+   
     if (this.isLoginMode) {
+      this.authObs= this.authService.login(
+        authForm.value.email,
+        authForm.value.password
+      );
+      // .subscribe();
     } else {
-      this.authService
-        .signup(authForm.value.email, authForm.value.password)
-        .subscribe(
-          (response: any) => {
-            console.log(response);
-            this.isLoading = false;
-          },
-          (error: any) => {
-            console.log(error);
-            this.isLoading = false;
-            this.error="An Error Ocurred"
-          }
-        );
+      this.authObs = this.authService.signup(
+        authForm.value.email,
+        authForm.value.password
+      );
+     
     }
+    this.authObs.subscribe(
+        (response: any) => {
+          console.log(response);
+          this.isLoading = false;
+        },  
+        (errorMessage: any) => {
+          this.error = errorMessage;
+          this.isLoading = false;
+        }
+    )
   }
+
+
+
   // alternative way of validation other validattion of gamil in html file
   getPasswordErrors(password: FormControl | NgModel): any {
     if (password?.errors?.['required']) {
--- src/app/auth/firebase.service.ts
+++ src/app/auth/firebase.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { catchError, Observable, throwError } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
@@ -11,27 +11,44 @@ export class FirestoreService {
   refreshToken: any;
   expiresIn: any;
   localId: any;
-
+  registered?: boolean;
   constructor(private http: HttpClient) {}
   isLoggedIn = false;
- 
+
   signup(email: string, password: string): Observable<any> {
-    return this.http.post(
-      `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyByah49euVrS1uh_XEmUI-oQW7Aa9HYEMY`,
+    return this.http
+      .post(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyByah49euVrS1uh_XEmUI-oQW7Aa9HYEMY`,
+        { email, password, returnSecureToken: true }
+      )
+      .pipe(
+        catchError((errorRes: any) => {
+          let errorMessage = 'An Error Ocurred';
+          if (!errorRes.error || !errorRes.error.error) {
+            return throwError(errorMessage);
+          }
+          switch (errorRes.error.error.message) {
+            case 'EMAIL_EXISTS':
+              errorMessage = 'Email Already Exist ';
+          }
+          return throwError(errorMessage);
+        })
+      );
+  }
+  login(email: string, password: string) {
+    // this.isLoggedIn = true;
+    this.http.post(
+      `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyByah49euVrS1uh_XEmUI-oQW7Aa9HYEMY`,
       { email, password, returnSecureToken: true }
     );
   }
-  login() {
-    this.isLoggedIn = true;
-  }
   logout() {
-    this.isLoggedIn = false;
+    // this.isLoggedIn = false;
   }
   isAuthentication() {
     return new Promise((reslove, reject) => {
       setTimeout(() => {
         reslove(this.isLoggedIn);
-        
       }, 1000);
     });
   }
